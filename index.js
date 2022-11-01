8624a6861debb2c1ede0eecffb8c68b0203f277a
skoziuberda <severyn.koziuberda@dataart.com>
2022-11-01 14:56:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 80;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port);
+console.log('Server is listening on port ' + port)
